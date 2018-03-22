d4446f26585f7733a0eb3cb8eb7047742b4a14a4
Hassaram Ramchandani <hramchandani@bblbze.com>
2018-03-22 15:47:10 -0600

    Day 5 Exercise

---
+++ day05-homework/atmclass.ts
@@ -0,0 +1,23 @@
+import { AtmInterface } from "./atminterface";
+
+export class AtmClass implements AtmInterface {
+
+    currentBalance: number;
+
+    setInitialBalance(arg: number): void {
+        this.currentBalance = arg;
+    }
+
+    viewBalance(): void {
+        console.log("Balance is " + this.currentBalance);
+    }
+
+    deposit(arg: number): void {
+        this.currentBalance = this.currentBalance + arg;
+    }
+
+    withdraw(arg: number): void {
+        this.currentBalance = this.currentBalance - arg;
+    }
+
+}
---
+++ day05-homework/atmuseclass.ts
@@ -0,0 +1,12 @@
+import { AtmClass } from "./atmclass";
+
+let atm001 = new AtmClass;
+
+atm001.setInitialBalance(750);
+atm001.viewBalance();
+
+atm001.deposit(20);
+atm001.viewBalance();
+
+atm001.withdraw(35);
+atm001.viewBalance();
